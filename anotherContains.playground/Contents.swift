dbe91b26b11d8d4bbcc87fd41d6d7fb5b51bf52a
Breena <breena_greek@live.com>
2020-05-04 22:56:38 -0400

    Progress

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -1,12 +1,17 @@
 import Foundation
 
 extension String {
-    
+    func anotherContains(to aString:String) -> Bool {
+        if self.caseInsensitiveCompare(aString) == .orderedSame {
+            return true
+        } else {
+            return false
+        }
+    }
 }
 
-
 // test cases
-print("Where is WaLdO".anotherContains("WALDO")) // true
-print("Where is WaLdO".anotherContains("where")) // true
-print("Where is WaLdO".anotherContains("is wA")) // true
-print("Where is WaLdO".anotherContains("nOPe"))  // false
+print("Where is WaLdO".anotherContains(to: "WALDO")) // true
+print("Where is WaLdO".anotherContains(to: "where")) // true
+print("Where is WaLdO".anotherContains(to: "is wA")) // true
+print("Where is WaLdO".anotherContains(to: "nOPe"))  // false
