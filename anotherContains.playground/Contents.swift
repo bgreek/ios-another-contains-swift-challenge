9df64f4cea06255490e4f3d84b38b5bfdeaf6adf
Breena <breena_greek@live.com>
2020-05-04 23:19:06 -0400

    Finished

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -1,19 +1,29 @@
 import Foundation
 
+//extension String {
+//    func anotherContains(to aString:String) -> Bool {
+//        if aString.caseInsensitiveCompare(self) == .orderedSame {
+//            return true
+//        } else {
+//            return false
+//        }
+//    }
+//}
+
+
 extension String {
-    func anotherContains(to aString:String) -> Bool {
-        if aString.caseInsensitiveCompare(self) == .orderedSame {
-            return true
+    func anotherContains(to aString: String) -> Bool {
+        if self.localizedLowercase.range(of: aString.localizedLowercase) != nil {
+             return true
         } else {
-            return false
+             return false
         }
     }
 }
-
 // test cases
 print("Where is WaLdO".anotherContains(to: "WALDO")) // true
 print("Where is WaLdO".anotherContains(to: "where")) // true
 print("Where is WaLdO".anotherContains(to: "is wA")) // true
 print("Where is WaLdO".anotherContains(to: "nOPe"))  // false
 
-print("HI".anotherContains(to: "HI"))
+print("HI there".anotherContains(to: "HI"))
